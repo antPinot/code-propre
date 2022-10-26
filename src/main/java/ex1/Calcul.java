497bef311a7bea6ffa1b6743c3cb78cf3b24ed39
Anthony Pinot <pinot.anthony@gmail.com>
2022-10-26 11:53:55 +0200

    Ex 1

--- src/main/java/ex1/Calcul.java
+++ src/main/java/ex1/Calcul.java
@@ -1,12 +1,31 @@
 package ex1;
 
-public class calcul {
+/**
+ * @author antPinot
+ * 
+ * Cette classe permet de réaliser un calcul de base : 
+ * une addition ou une soustraction
+ *
+ */
+public class Calcul {
 
-	public int get1(int a, int b){
+	/**
+	 * Renvoie la somme de a et b
+	 * @param a : entier 1
+	 * @param b : entier 2
+	 * @return int
+	 */
+	public int addition(int a, int b){
 		return a+b;
 	}
 	
-	public int get2(int a, int b){
+	/**
+	 * Renvoie la différence de a et b
+	 * @param a entier 1
+	 * @param b entier 2
+	 * @return int
+	 */
+	public int soustraction(int a, int b){
 		return a-b;
 	}
 }
--- src/main/java/ex1/Entreprise.java
+++ src/main/java/ex1/Entreprise.java
@@ -2,17 +2,98 @@ package ex1;
 
 import java.util.Date;
 
-public class entreprise {
+/**
+ * @author antPinot
+ * 
+ * Cette méthode abstraite décrit le concept d'Entreprise
+ *
+ */
+public abstract class Entreprise {
 
-	public int Siret;
-	public String Nom;
-	public String adresse;
-	public Date date_Creation;
+	//Attribut donnant le n° SIRET 
+	private int siret;
+
+	//Attribut donnant le nom
+	private String nom;
+
+	//Attribut donnant l'adresse
+	private String adresse;
+
+	//Attribut donnant la date de création
+	private Date dateCreation;
 	
-	public static final int capitalMax = 3000000;
+
+	//Attribut (constante) donnant le capital maximum d'une entreprise
+	public static final int CAPITAL_MAX = 3000000;
 	
-	public void Afficher_statut(){
-		
+	/**
+	 * Méthode abstraite qui affiche le statut et ne renvoie rien
+	 * 
+	 */
+	public abstract void Afficher_statut();
+
+	/**Getter pour l'attribut siret
+	 * @return the siret
+	 */
+	public int getSiret() {
+		return siret;
 	}
+
+	/**Setter pour l'attribut siret
+	 * @param siret the siret to set
+	 */
+	public void setSiret(int siret) {
+		this.siret = siret;
+	}
+
+	/**Getter pour l'attribut nom
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**Setter pour l'attribut nom
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	/**Getter pour l'attribut adresse
+	 * @return the adresse
+	 */
+	public String getAdresse() {
+		return adresse;
+	}
+
+	/**Setter pour l'attribut adresse
+	 * @param adresse the adresse to set
+	 */
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	/**Getter pour l'attribut dateCreation
+	 * @return the dateCreation
+	 */
+	public Date getDateCreation() {
+		return dateCreation;
+	}
+
+	/**Setter pour l'attribut dateCreation
+	 * @param dateCreation the dateCreation to set
+	 */
+	public void setDateCreation(Date dateCreation) {
+		this.dateCreation = dateCreation;
+	}
+
+	/**Getter pour l'attribut capitalMax
+	 * @return the capitalMax
+	 */
+	public static int getCapitalMax() {
+		return CAPITAL_MAX;
+	}
+	
 	
 }
